b052384a1e681c9e9fcaa58ea2e796db1d3df393
Stefanie Lima <stefanielima_araujo@hotmail.com>
2021-10-02 00:57:23 -0300

    feat: add metatags and favicon

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import styled from 'styled-components'
+import Head from 'next/head'
 
 import Sidebar from '../components/Sidebar'
 
-export default function Home () {
-  return (
-    <>
-      {/* <Title>My page</Title> */}
-      <Sidebar/>
-    </>
-  )
-}
+const Home = () =>
+  <>
+    <Head>
+      <title>stelima</title>
+      <link rel='icon' type='image/png' href='/photo.png' />
+      <meta name='description' content='Desenvolvedora Front-end'/>
+      <meta property='og:title' content='Stefanie Lima'/>
+      <meta property='og:description' content='Desenvolvedora Front-end'/>
+      <meta property='og:url' content='https://stelima.dev'/>
+      <meta property='og:image' content='/photo.png'/>
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png'/>
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png'/>
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png'/>
+    </Head>
+    <Sidebar/>
+  </>
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export default Home
